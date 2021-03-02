86b69e0480ca210bbc7499199bdb0d6f5c454d67
masiyematome <matomemasiye7@gmail.com>
2021-03-02 18:20:38 +0200

    Added code that allows us to view less/more of the specification text on the click of a link

--- items_app.js
+++ items_app.js
@@ -8,10 +8,27 @@ const StoreItemList = document.querySelectorAll(".store-item");
 
     StoreItemList.forEach(storeItem => {
         Array.from(storeItem.children).forEach(individualChild => {
-            if(individualChild.classList.contains("item-spec")){
+            if (individualChild.classList.contains("item-spec")) {
                 individualChild.textContent = shortSpecificationsText;
             }
 
+            else if (individualChild.classList.contains("see-more")) {
+                individualChild.addEventListener("click", (ev) => {
+                    ev.preventDefault();
+
+                    if (individualChild.textContent == "see more") {
+                        storeItem.children[1].textContent = fullSpecificationsText;
+                        individualChild.textContent = "see less"
+
+                    }
+
+                    else if (individualChild.textContent == "see less") {
+                        storeItem.children[1].textContent = shortSpecificationsText;
+                        individualChild.textContent = "see more"
+                    }
+
+                })
+            }
         })
     })
 
