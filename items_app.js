36e418904cbe605bb98cff263dbad4c37b284e9a
masiyematome <matomemasiye7@gmail.com>
2021-03-09 13:29:03 +0200

    A function that takes us to the index of the clicked button

--- items_app.js
+++ items_app.js
@@ -1,6 +1,16 @@
 
+//Selectors
 const storeItems = document.querySelectorAll(".store-item");
 const itemSpecs = document.querySelectorAll(".item-spec");
+const pagination = document.querySelector(".pagination");
+let currentIndex = 0;
+
+
+//Event Listeners
+
+pagination.addEventListener("click" , paginate);
+
+//Functions
 
 /**Specification text**/
 
@@ -45,3 +55,47 @@ storeItems.forEach(storeItem => {
 });
 
 /**Pagination starts here**/
+
+function paginate(ev){
+    ev.preventDefault();
+    const clickedButton = ev.target;
+
+
+    if(clickedButton.classList.contains("previous")){
+        if(currentIndex == 0){
+            console.log("I am at " + currentIndex + " so, I can not go back");
+        }
+
+        else{
+            currentIndex = currentIndex - 1;
+            console.log(currentIndex);
+        }
+    }
+
+    else if(clickedButton.classList.contains("first-page")){
+        currentIndex = 0;
+        console.log("I am at index " + currentIndex);
+    }
+
+    else if(clickedButton.classList.contains("second-page")){
+        currentIndex = 1;
+        console.log("I am at index " + currentIndex);
+    }
+
+    else if(clickedButton.classList.contains("third-page")){
+        currentIndex = 2;
+        console.log("I am at index " + currentIndex);
+    }
+
+    else if(clickedButton.classList.contains("next")){
+        if(currentIndex == 2) {
+            console.log("I am " + currentIndex + " So i can not go forward");
+        }
+
+        else{
+            currentIndex = currentIndex + 1;
+            console.log(currentIndex);
+        }
+    }
+
+}
