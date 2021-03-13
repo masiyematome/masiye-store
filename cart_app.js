f3d2da64190c651851ee158d0c5abfba20bc6a7a
masiyematome <matomemasiye7@gmail.com>
2021-03-13 18:54:23 +0200

    Code that notifies the user about the clicked item being added to cart

--- cart_app.js
+++ cart_app.js
@@ -12,9 +12,18 @@ storeItems.forEach((storeItem) => {
         
         const clicked = e.target;
 
-        if(clicked.classList.contains("add-to-cart")){
+        if (clicked.classList.contains("add-to-cart")) {
             numberOfItems = numberOfItems + 1;
             totalPrice = totalPrice + parseFloat(storeItem.children[4].children[0].innerText);
+            clicked.classList.add("added-to-cart");
+
+            clicked.innerHTML = '<i class = "fa fa-check"></i>' + "Added to Cart";
+
+            setTimeout(() => {
+                clicked.classList.remove("added-to-cart");
+                clicked.innerHTML = '<i class = "fa fa-cart-plus"></i>' + 'Add to cart';
+            }, 1500);
+
         }
 
         numberOfItemsText.innerText = numberOfItems + " Items - R";
